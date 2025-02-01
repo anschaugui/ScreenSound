954b0d73273fa5afbfe151a2ae82e844785681ba
Guilherme Anschau <anschau.gui@gmail.com>
2025-02-01 17:22:43 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+Musica musica1 = new Musica();
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+musica1.duracao = 273;
+musica1.disponivel=  true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigol";
+musica2.artista = "U2";
+musica2.duracao = 367;
+musica2.disponivel=  false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+
+
+
+
+
