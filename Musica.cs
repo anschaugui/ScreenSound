f06db20f0e543bc5fd2b78de08a8fbd4e5567429
Guilherme Anschau <anschau.gui@gmail.com>
2025-02-02 07:45:14 -0300

    propriedades

--- Musica.cs
+++ Musica.cs
@@ -2,27 +2,18 @@ using System.Text.RegularExpressions;
 
 class Musica
 {
-public string nome; 
-public string artista; 
-public int duracao;
-private bool disponivel; 
-
-public void EscreveDisponivel(bool value)
-{
-    disponivel = value; 
-}
-
-public bool LeDisponivel()
-{
-    return disponivel;
-}
+public string Nome {get; set;} 
+public string Artista{get ; set;} 
+public int Duracao{get ; set;}
+public bool Disponivel { get; set;}
+public string NomeCompleto { get; set; }
 
 public void ExibirFichaTecnica()
 {
-    Console.WriteLine($"Nome: {nome}");
-    System.Console.WriteLine($"Artista: {artista}");
-    System.Console.WriteLine($"Duração: {duracao}");
-    if (disponivel)
+    Console.WriteLine($"Nome: {Nome}");
+    System.Console.WriteLine($"Artista: {Artista}");
+    System.Console.WriteLine($"Duração: {Duracao}");
+    if (Disponivel)
     {
             System.Console.WriteLine($"Disponivel no plano");
     }else
--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
 ﻿using System.Runtime.InteropServices;
 
-Musica musica1 = new Musica();
-musica1.nome = "Roxane";
-musica1.artista = "The Police";
-musica1.duracao = 273;
-musica1.EscreveDisponivel(true);
-System.Console.WriteLine(musica1.LeDisponivel());
+Musica musica1 = new()
+{
+    Nome = "Roxane",
+    Artista = "The Police",
+    Duracao = 273,
+    Disponivel = true
+};
+System.Console.WriteLine(musica1.Disponivel);
 
 Musica musica2 = new Musica();
-musica2.nome = "Vertigol";
-musica2.artista = "U2";
-musica2.duracao = 367;
-musica2.EscreveDisponivel(false);
-System.Console.WriteLine(musica2.LeDisponivel());
+musica2.Nome = "Vertigol";
+musica2.Artista = "U2";
+musica2.Duracao = 367;
+musica2.Disponivel = false;
+System.Console.WriteLine(musica2.Disponivel);
 musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
 
